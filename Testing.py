031eb720888b1908e2b1fada3257e51709163198
Eddie Pantridge <EPantridge@MassMutual.com>
2016-06-18 00:55:28 -0400

    Change Testing.py demo file

--- Testing.py
+++ Testing.py
@@ -1,6 +1,8 @@
 import pysh_random
 import pysh_interpreter
+
 import pysh_plush_translation as translate
+import pysh_globals as g
 
 gn = pysh_random.random_plush_genome(50)
 print gn
@@ -14,6 +16,7 @@ interpreter = pysh_interpreter.Pysh_Interpreter()
 interpreter.run_push(prog)
 interpreter.state.pretty_print()
 
+print g.pysh_argmap['atom_generators']
 
 ######################################
 # Broken programs

068cd3c49d399c1783cf865110e5749cce04e544
Eddie Pantridge <EPantridge@MassMutual.com>
2016-06-18 01:07:44 -0400

    Added comments to Testing.py demo file

--- Testing.py
+++ Testing.py
@@ -4,20 +4,16 @@ import pysh_interpreter
 import pysh_plush_translation as translate
 import pysh_globals as g
 
-gn = pysh_random.random_plush_genome(50)
-print gn
+gn = pysh_random.random_plush_genome(50)						# Generation a plush genome with a max size of 50
+print "PLUSH GENOME:\n", gn
 print
 
-prog = translate.translate_plush_genome_to_push_program(gn)
-print prog
+prog = translate.translate_plush_genome_to_push_program(gn)		# Translate plush genome to push program
+print "PUSH PROGRAM:\n", prog
 print
 
-interpreter = pysh_interpreter.Pysh_Interpreter()
-interpreter.run_push(prog)
+interpreter = pysh_interpreter.Pysh_Interpreter()				# Create instance of a Pysh_Interpreter.
+interpreter.run_push(prog)										# Run program.
+print "PROGRAM OUTPUT:"
 interpreter.state.pretty_print()
-
-print g.pysh_argmap['atom_generators']
-
-######################################
-# Broken programs
 

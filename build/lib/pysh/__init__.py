0f6909a17278b33e10a044af4efc41408d02b354
Eddie Pantridge <EPantridge@MassMutual.com>
2016-10-07 11:37:18 -0400

    .gitignore is now working

--- build/lib/pysh/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Jun  5 15:34:38 2016
-
-@author: Eddie
-"""
-from __future__ import absolute_import, division, print_function, unicode_literals
-
-# Forces this directory to become a python package.

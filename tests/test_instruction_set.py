21b3601901c75a415f65e5631e77a631925f7e09
Eddie Pantridge <EPantridge@MassMutual.com>
2016-10-10 10:17:49 -0400

    Fix mini test

--- tests/test_instruction_set.py
+++ tests/test_instruction_set.py
@@ -11,41 +11,44 @@ i = interp.Pysh_Interpreter()
 print("Testing boolean_and")
 prog = [True, False, ri.get_instruction_by_name('boolean_and')]
 result = i.run_push(prog)
-assert i.state.total_state_size() == 1,
-       "State has %r items. Should be 1." % i.state.total_state_size()
-assert len(i.state.stacks['_boolean']) == 1,
-       i.state.stacks['_boolean']
-assert i.state.stacks['_boolean'].top_item() == False,
-       i.state.stacks['_boolean'].top_item()
+if not i.state.total_state_size() == 1:
+    raise Exception("State has %r items. Should be 1." % i.state.total_state_size())
+assert len(i.state.stacks['_boolean']) == 1
+assert i.state.stacks['_boolean'].top_item() == False
 
 prog = [True, True, ri.get_instruction_by_name('boolean_and')]
 result = i.run_push(prog)
-assert i.state.total_state_size() == 1,                     "State has %r items. Should be 1." % i.state.total_state_size()
-assert len(i.state.stacks['_boolean']) == 1                 i.state.stacks['_boolean']
-assert i.state.stacks['_boolean'].top_item() == True        i.state.stacks['_boolean'].top_item()
+if not i.state.total_state_size() == 1:
+    raise Exception("State has %r items. Should be 1." % i.state.total_state_size())
+assert len(i.state.stacks['_boolean']) == 1
+assert i.state.stacks['_boolean'].top_item() == True
 
 print("Testing boolean_or")
 prog = [True, False, ri.get_instruction_by_name('boolean_or')]
 result = i.run_push(prog)
-assert i.state.total_state_size() == 1,                     "State has %r items. Should be 1." % i.state.total_state_size()
-assert len(i.state.stacks['_boolean']) == 1                 i.state.stacks['_boolean']
-assert i.state.stacks['_boolean'].top_item() == True        i.state.stacks['_boolean'].top_item()
+if not i.state.total_state_size() == 1:
+    raise Exception("State has %r items. Should be 1." % i.state.total_state_size())
+assert len(i.state.stacks['_boolean']) == 1
+assert i.state.stacks['_boolean'].top_item() == True
 
 prog = [False, False, ri.get_instruction_by_name('boolean_or')]
 result = i.run_push(prog)
-assert i.state.total_state_size() == 1,                     "State has %r items. Should be 1." % i.state.total_state_size()
-assert len(i.state.stacks['_boolean']) == 1                 i.state.stacks['_boolean']
-assert i.state.stacks['_boolean'].top_item() == False       i.state.stacks['_boolean'].top_item()
+if not i.state.total_state_size() == 1:
+    raise Exception("State has %r items. Should be 1." % i.state.total_state_size())
+assert len(i.state.stacks['_boolean']) == 1
+assert i.state.stacks['_boolean'].top_item() == False
 
 print("Testing boolean_not")
 prog = [True, ri.get_instruction_by_name('boolean_not')]
 result = i.run_push(prog)
-assert i.state.total_state_size() == 1,                     "State has %r items. Should be 1." % i.state.total_state_size()
-assert len(i.state.stacks['_boolean']) == 1                 i.state.stacks['_boolean']
-assert i.state.stacks['_boolean'].top_item() == False       i.state.stacks['_boolean'].top_item()
+if not i.state.total_state_size() == 1:
+    raise Exception("State has %r items. Should be 1." % i.state.total_state_size())
+assert len(i.state.stacks['_boolean']) == 1
+assert i.state.stacks['_boolean'].top_item() == False
 
 prog = [False, ri.get_instruction_by_name('boolean_not')]
 result = i.run_push(prog)
-assert i.state.total_state_size() == 1,                     "State has %r items. Should be 1." % i.state.total_state_size()
-assert len(i.state.stacks['_boolean']) == 1                 i.state.stacks['_boolean']
-assert i.state.stacks['_boolean'].top_item() == True        i.state.stacks['_boolean'].top_item()
+if not i.state.total_state_size() == 1:
+    raise Exception("State has %r items. Should be 1." % i.state.total_state_size())
+assert len(i.state.stacks['_boolean']) == 1
+assert i.state.stacks['_boolean'].top_item() == True

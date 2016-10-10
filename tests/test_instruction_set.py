d952801012de8daaf39d7d6d3f819cc60aab99c3
Eddie Pantridge <EPantridge@MassMutual.com>
2016-10-10 09:48:31 -0400

    Added first mini-test of instructions

--- tests/test_instruction_set.py
+++ tests/test_instruction_set.py
@@ -1,1 +1,30 @@
-print("Tests are working!")
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+import pysh.pysh_interpreter as interp
+from pysh.instructions import boolean, char, code, common, numbers, string, input_output
+from pysh.instructions import registered_instructions as ri
+
+print("Setting up Pysh interpreter...")
+interpreter = interp.Pysh_Interpreter()
+
+print("Testing boolean_and")
+prog = [True, False, ri.get_instruction_by_name('boolean_and')]
+result = interpreter.run_push(prog)
+assert len(interpreter.state.stacks['_boolean']) == 1
+assert interpreter.state.stacks['_boolean'].top_item() == False
+
+prog = [True, True, ri.get_instruction_by_name('boolean_and')]
+result = interpreter.run_push(prog)
+assert len(interpreter.state.stacks['_boolean']) == 1
+assert interpreter.state.stacks['_boolean'].top_item() == True
+
+print("Testing boolean_or")
+prog = [True, False, ri.get_instruction_by_name('boolean_or')]
+result = interpreter.run_push(prog)
+assert len(interpreter.state.stacks['_boolean']) == 1
+assert interpreter.state.stacks['_boolean'].top_item() == True
+
+prog = [False, False, ri.get_instruction_by_name('boolean_or')]
+result = interpreter.run_push(prog)
+assert len(interpreter.state.stacks['_boolean']) == 1
+assert interpreter.state.stacks['_boolean'].top_item() == False

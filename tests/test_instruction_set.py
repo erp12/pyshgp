8eaba02ae93058f791a1ab48abc53b765966ecba
Eddie Pantridge <EPantridge@MassMutual.com>
2016-10-10 10:03:21 -0400

    Added more usefull printing to mini test

--- tests/test_instruction_set.py
+++ tests/test_instruction_set.py
@@ -4,44 +4,45 @@ import pysh.pysh_interpreter as interp
 from pysh.instructions import boolean, char, code, common, numbers, string, input_output
 from pysh.instructions import registered_instructions as ri
 
+
 print("Setting up Pysh interpreter...")
-interpreter = interp.Pysh_Interpreter()
+i = interp.Pysh_Interpreter()
 
 print("Testing boolean_and")
 prog = [True, False, ri.get_instruction_by_name('boolean_and')]
-result = interpreter.run_push(prog)
-assert interpreter.state.total_state_size() == 1
-assert len(interpreter.state.stacks['_boolean']) == 1
-assert interpreter.state.stacks['_boolean'].top_item() == False
+result = i.run_push(prog)
+assert i.state.total_state_size() == 1,						"State has %r items. Should be 1." % i.state.total_state_size()
+assert len(i.state.stacks['_boolean']) == 1, 				i.state.stacks['_boolean']
+assert i.state.stacks['_boolean'].top_item() == False, 		i.state.stacks['_boolean'].top_item()
 
 prog = [True, True, ri.get_instruction_by_name('boolean_and')]
-result = interpreter.run_push(prog)
-assert interpreter.state.total_state_size() == 1
-assert len(interpreter.state.stacks['_boolean']) == 1
-assert interpreter.state.stacks['_boolean'].top_item() == True
+result = i.run_push(prog)
+assert i.state.total_state_size() == 1,						"State has %r items. Should be 1." % i.state.total_state_size()
+assert len(i.state.stacks['_boolean']) == 1					i.state.stacks['_boolean']
+assert i.state.stacks['_boolean'].top_item() == True		i.state.stacks['_boolean'].top_item()
 
 print("Testing boolean_or")
 prog = [True, False, ri.get_instruction_by_name('boolean_or')]
-result = interpreter.run_push(prog)
-assert interpreter.state.total_state_size() == 1
-assert len(interpreter.state.stacks['_boolean']) == 1
-assert interpreter.state.stacks['_boolean'].top_item() == True
+result = i.run_push(prog)
+assert i.state.total_state_size() == 1,						"State has %r items. Should be 1." % i.state.total_state_size()
+assert len(i.state.stacks['_boolean']) == 1					i.state.stacks['_boolean']
+assert i.state.stacks['_boolean'].top_item() == True		i.state.stacks['_boolean'].top_item()
 
 prog = [False, False, ri.get_instruction_by_name('boolean_or')]
-result = interpreter.run_push(prog)
-assert interpreter.state.total_state_size() == 1
-assert len(interpreter.state.stacks['_boolean']) == 1
-assert interpreter.state.stacks['_boolean'].top_item() == False
+result = i.run_push(prog)
+assert i.state.total_state_size() == 1,						"State has %r items. Should be 1." % i.state.total_state_size()
+assert len(i.state.stacks['_boolean']) == 1					i.state.stacks['_boolean']
+assert i.state.stacks['_boolean'].top_item() == False		i.state.stacks['_boolean'].top_item()
 
 print("Testing boolean_not")
 prog = [True, ri.get_instruction_by_name('boolean_not')]
-result = interpreter.run_push(prog)
-assert interpreter.state.total_state_size() == 1
-assert len(interpreter.state.stacks['_boolean']) == 1
-assert interpreter.state.stacks['_boolean'].top_item() == False
+result = i.run_push(prog)
+assert i.state.total_state_size() == 1,						"State has %r items. Should be 1." % i.state.total_state_size()
+assert len(i.state.stacks['_boolean']) == 1					i.state.stacks['_boolean']
+assert i.state.stacks['_boolean'].top_item() == False		i.state.stacks['_boolean'].top_item()
 
 prog = [False, ri.get_instruction_by_name('boolean_not')]
-result = interpreter.run_push(prog)
-assert interpreter.state.total_state_size() == 1
-assert len(interpreter.state.stacks['_boolean']) == 1
-assert interpreter.state.stacks['_boolean'].top_item() == True
+result = i.run_push(prog)
+assert i.state.total_state_size() == 1,						"State has %r items. Should be 1." % i.state.total_state_size()
+assert len(i.state.stacks['_boolean']) == 1					i.state.stacks['_boolean']
+assert i.state.stacks['_boolean'].top_item() == True		i.state.stacks['_boolean'].top_item()

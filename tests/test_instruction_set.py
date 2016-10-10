4aee05759ad721c4627783c52177656799bcf8ec
Eddie Pantridge <EPantridge@MassMutual.com>
2016-10-10 10:05:45 -0400

    Formatting test

--- tests/test_instruction_set.py
+++ tests/test_instruction_set.py
@@ -11,38 +11,38 @@ i = interp.Pysh_Interpreter()
 print("Testing boolean_and")
 prog = [True, False, ri.get_instruction_by_name('boolean_and')]
 result = i.run_push(prog)
-assert i.state.total_state_size() == 1,						"State has %r items. Should be 1." % i.state.total_state_size()
-assert len(i.state.stacks['_boolean']) == 1, 				i.state.stacks['_boolean']
-assert i.state.stacks['_boolean'].top_item() == False, 		i.state.stacks['_boolean'].top_item()
+assert i.state.total_state_size() == 1,                     "State has %r items. Should be 1." % i.state.total_state_size()
+assert len(i.state.stacks['_boolean']) == 1,                i.state.stacks['_boolean']
+assert i.state.stacks['_boolean'].top_item() == False,      i.state.stacks['_boolean'].top_item()
 
 prog = [True, True, ri.get_instruction_by_name('boolean_and')]
 result = i.run_push(prog)
-assert i.state.total_state_size() == 1,						"State has %r items. Should be 1." % i.state.total_state_size()
-assert len(i.state.stacks['_boolean']) == 1					i.state.stacks['_boolean']
-assert i.state.stacks['_boolean'].top_item() == True		i.state.stacks['_boolean'].top_item()
+assert i.state.total_state_size() == 1,                     "State has %r items. Should be 1." % i.state.total_state_size()
+assert len(i.state.stacks['_boolean']) == 1                 i.state.stacks['_boolean']
+assert i.state.stacks['_boolean'].top_item() == True        i.state.stacks['_boolean'].top_item()
 
 print("Testing boolean_or")
 prog = [True, False, ri.get_instruction_by_name('boolean_or')]
 result = i.run_push(prog)
-assert i.state.total_state_size() == 1,						"State has %r items. Should be 1." % i.state.total_state_size()
-assert len(i.state.stacks['_boolean']) == 1					i.state.stacks['_boolean']
-assert i.state.stacks['_boolean'].top_item() == True		i.state.stacks['_boolean'].top_item()
+assert i.state.total_state_size() == 1,                     "State has %r items. Should be 1." % i.state.total_state_size()
+assert len(i.state.stacks['_boolean']) == 1                 i.state.stacks['_boolean']
+assert i.state.stacks['_boolean'].top_item() == True        i.state.stacks['_boolean'].top_item()
 
 prog = [False, False, ri.get_instruction_by_name('boolean_or')]
 result = i.run_push(prog)
-assert i.state.total_state_size() == 1,						"State has %r items. Should be 1." % i.state.total_state_size()
-assert len(i.state.stacks['_boolean']) == 1					i.state.stacks['_boolean']
-assert i.state.stacks['_boolean'].top_item() == False		i.state.stacks['_boolean'].top_item()
+assert i.state.total_state_size() == 1,                     "State has %r items. Should be 1." % i.state.total_state_size()
+assert len(i.state.stacks['_boolean']) == 1                 i.state.stacks['_boolean']
+assert i.state.stacks['_boolean'].top_item() == False       i.state.stacks['_boolean'].top_item()
 
 print("Testing boolean_not")
 prog = [True, ri.get_instruction_by_name('boolean_not')]
 result = i.run_push(prog)
-assert i.state.total_state_size() == 1,						"State has %r items. Should be 1." % i.state.total_state_size()
-assert len(i.state.stacks['_boolean']) == 1					i.state.stacks['_boolean']
-assert i.state.stacks['_boolean'].top_item() == False		i.state.stacks['_boolean'].top_item()
+assert i.state.total_state_size() == 1,                     "State has %r items. Should be 1." % i.state.total_state_size()
+assert len(i.state.stacks['_boolean']) == 1                 i.state.stacks['_boolean']
+assert i.state.stacks['_boolean'].top_item() == False       i.state.stacks['_boolean'].top_item()
 
 prog = [False, ri.get_instruction_by_name('boolean_not')]
 result = i.run_push(prog)
-assert i.state.total_state_size() == 1,						"State has %r items. Should be 1." % i.state.total_state_size()
-assert len(i.state.stacks['_boolean']) == 1					i.state.stacks['_boolean']
-assert i.state.stacks['_boolean'].top_item() == True		i.state.stacks['_boolean'].top_item()
+assert i.state.total_state_size() == 1,                     "State has %r items. Should be 1." % i.state.total_state_size()
+assert len(i.state.stacks['_boolean']) == 1                 i.state.stacks['_boolean']
+assert i.state.stacks['_boolean'].top_item() == True        i.state.stacks['_boolean'].top_item()

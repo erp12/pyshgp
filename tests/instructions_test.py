ca75882f27fd81e60271a5ed93b9071b8c72c46f
erp12 <EPantridge@MassMutual.com>
2017-04-10 01:58:00 -0400

    Fixed instruction unit tests

--- tests/instructions_test.py
+++ tests/instructions_test.py
@@ -6,22 +6,22 @@ import pyshgp.push.instruction as instr
 import pyshgp.push.instructions.registered_instructions as ri
 from pyshgp.push.instructions import *
 
-def dict_matches_state(push_state, state_dict):
+def dict_matches_state(interpreter, state_dict):
 	i = 0
 	for k in state_dict.keys():
-		if not state_dict[k][:] == push_state.stacks[k][:]:
+		if not state_dict[k][:] == interpreter.state[k][:]:
 			return False
 		i += len(state_dict[k])
 	if '_output' in state_dict.keys():
 		i -= 1
-	if i == push_state.size():
+	if i == interpreter.state_size():
 		return True
 	else:
 		return False
 
 def run_test(test_info, print_test=False):
 	interpreter = interp.PushInterpreter()
-	interpreter.load_state(test_info[0])
+	interpreter.state_from_dict(test_info[0])
 
 	instruction = test_info[1]
 	if not isinstance(test_info[1], instr.PyshInstruction):
@@ -30,15 +30,15 @@ def run_test(test_info, print_test=False):
 	if print_test:
 		print(instruction.name)
 		print("Before:")
-		interpreter.state.pretty_print()
+		interpreter.state_pretty_print()
 
 	if type(test_info[2]) == dict:
 		interpreter.execute_instruction(instruction)
 		if print_test:
 			print("After:")
-			interpreter.state.pretty_print()
+			interpreter.state_pretty_print()
 			print()
-		return dict_matches_state(interpreter.state, test_info[2])
+		return dict_matches_state(interpreter, test_info[2])
 	else:
 		try:
 			interpreter.execute_instruction(instruction)

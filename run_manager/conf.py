9e7235b5213b3cdf6c4a15be158675081a02eaa7
erp12 <EPantridge@MassMutual.com>
2017-09-21 16:24:23 -0400

    fix path

--- run_manager/conf.py
+++ run_manager/conf.py
@@ -2,7 +2,7 @@
 
 # FLY
 FLY_PYSHGP_PATH = '/home/erp12/pyshgp'
-FLY_JOB_TEMPLATE = FLY_PYSHGP_PATH + '/benchmark/fly-template.alf'
+FLY_JOB_TEMPLATE = FLY_PYSHGP_PATH + '/run_manager/fly-template.alf'
 FLY_PYTHON_HOME = '/home/erp12/anaconda3/bin/python'
 FLY_LOG_PATH = '/home/erp12/gp_runs/pyshgp'
 

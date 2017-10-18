6fc2b57b904c8e13ed556c78ac94e7cab4d92ee1
erp12 <epantridge@massmutual.com>
2017-10-17 21:29:07 -0400

    .

--- run_manager/fly.py
+++ run_manager/fly.py
@@ -40,6 +40,13 @@ def start_fly_run(job_name: str, problem_file: str):
             run_line=run_line)
     with open('run_manager/pysh_job.alf', 'w') as f:
         f.write(job_cmd)
+    print(
+        ' '.join(
+            ['/opt/pixar/tractor-blade-1.7.2/python/bin/python2.6',
+             '/opt/pixar/tractor-blade-1.7.2/tractor-spool.py',
+             '--engine=fly:8000',
+             '{}/run_manager/pysh_job.alf'.format(FLY_PYSHGP_PATH)])
+    )
     os.system(' '.join(
         ['/opt/pixar/tractor-blade-1.7.2/python/bin/python2.6',
          '/opt/pixar/tractor-blade-1.7.2/tractor-spool.py',

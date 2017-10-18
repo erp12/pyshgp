de9861f3de2520238785cfd6f1815de115385768
erp12 <epantridge@massmutual.com>
2017-10-17 21:03:20 -0400

    fix path to tractor alf file

--- run_manager/fly.py
+++ run_manager/fly.py
@@ -44,7 +44,7 @@ def start_fly_run(job_name: str, problem_file: str):
         ['/opt/pixar/tractor-blade-1.7.2/python/bin/python2.6',
          '/opt/pixar/tractor-blade-1.7.2/tractor-spool.py',
          '--engine=fly:8000',
-         'pysh_job.alf']))
+         'run_manager/pysh_job.alf']))
 
 
 def start_n_fly_runs(problem_file, n_runs):

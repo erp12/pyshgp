66082dc60d31969bacad45a03d0c1fb3d1e1cff6
erp12 <eddie.pantridge@gmail.com>
2020-05-02 02:31:07 -0400

    Adds public API to init files for cleaner imports.

--- pyshgp/gp/__init__.py
+++ pyshgp/gp/__init__.py
@@ -1,1 +1,17 @@
 """pyshgp.push.gp."""
+
+from pyshgp.gp.estimators import PushEstimator
+from pyshgp.gp.search import SearchConfiguration, GeneticAlgorithm, SimulatedAnnealing
+from pyshgp.gp.genome import GeneSpawner
+from pyshgp.gp.evaluation import DatasetEvaluator, FunctionEvaluator
+
+
+__all__ = [
+    "PushEstimator",
+    "SearchConfiguration",
+    "GeneticAlgorithm",
+    "SimulatedAnnealing",
+    "GeneSpawner",
+    "DatasetEvaluator",
+    "FunctionEvaluator",
+]
--- pyshgp/push/__init__.py
+++ pyshgp/push/__init__.py
@@ -1,1 +1,12 @@
-"""pyshgp.push."""
+"""pyshgp.push"""
+
+from pyshgp.push.instruction_set import InstructionSet
+from pyshgp.push.interpreter import PushConfig, ProgramSignature, Program, PushInterpreter
+
+__all__ = [
+    "PushConfig",
+    "ProgramSignature",
+    "Program",
+    "PushInterpreter",
+    "InstructionSet",
+]

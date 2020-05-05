7c08a55b90caa863cdd67ee1c7fcc8d13c87ba72
erp12 <eddie.pantridge@gmail.com>
2020-05-05 00:20:16 -0400

    Adds ParallelContext to gp __init__

--- pyshgp/gp/__init__.py
+++ pyshgp/gp/__init__.py
@@ -1,7 +1,7 @@
 """pyshgp.push.gp."""
 
 from pyshgp.gp.estimators import PushEstimator
-from pyshgp.gp.search import SearchConfiguration, GeneticAlgorithm, SimulatedAnnealing
+from pyshgp.gp.search import SearchConfiguration, GeneticAlgorithm, SimulatedAnnealing, ParallelContext
 from pyshgp.gp.genome import GeneSpawner
 from pyshgp.gp.evaluation import DatasetEvaluator, FunctionEvaluator
 
@@ -9,6 +9,7 @@ from pyshgp.gp.evaluation import DatasetEvaluator, FunctionEvaluator
 __all__ = [
     "PushEstimator",
     "SearchConfiguration",
+    "ParallelContext",
     "GeneticAlgorithm",
     "SimulatedAnnealing",
     "GeneSpawner",
